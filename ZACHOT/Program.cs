93d04e7ed084d257540f4d61faba04d46144df1e
Кирилл Данилов <kopatich_16@mail.ru>
2021-05-19 20:42:08 +0700

    Добавлен метод для вычисления формулы

--- ZACHOT/Program.cs
+++ ZACHOT/Program.cs
@@ -10,7 +10,34 @@ namespace ZACHOT
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine($"{GetZ (1, 2, 3, 4)}");
+            Console.ReadKey();
+        }
+        public static double GetZ(double A, double B, double C, double D)
+        {
+            double max = 0;
+            double min = 0;
+            if (B>C)
+            {
+                max = B;
+            }
+            if (C>D+1)
+            {
+                max = C;
+            }
+            else
+            {
+                max = D + 1;
+            }
+            if (Math.Pow(A, 2)<max)
+            {
+                min = Math.Pow(A, 2);
+            }
+            else
+            {
+                min = max;
+            }
+            return min;
         }
     }
 }
